cd6ca805215e578bbce1565bed2af95cd15e450c
motaser <araidahmontaser@gmail.com>
2022-06-21 11:19:17 +0300

    Adding new Stuff to github

--- src/Anything.java
+++ src/Anything.java
@@ -1,6 +1,8 @@
 public class Anything {
     public static void main(String [] args){
-        System.out.println("Hello World");
+        for (int i = 0; i<9; i++){
+            System.out.println(i);
+        }
 
     }
 }
